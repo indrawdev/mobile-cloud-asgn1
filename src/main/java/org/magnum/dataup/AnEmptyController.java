04d1c6eca370578559b7441c9791829fbef7c729
Indra Pramana <indra@ide.web.id>
2021-03-20 14:04:20 +0700

    final code in AnEmptyController

--- src/main/java/org/magnum/dataup/AnEmptyController.java
+++ src/main/java/org/magnum/dataup/AnEmptyController.java
@@ -17,7 +17,31 @@
  */
 package org.magnum.dataup;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.magnum.dataup.model.Video;
+import org.magnum.dataup.model.VideoStatus;
+import org.magnum.dataup.model.VideoStatus.VideoState;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class AnEmptyController {
@@ -38,5 +62,59 @@ public class AnEmptyController {
                                                                                                                                                                                                                                                                         
 	 * 
 	 */
+	private static final AtomicLong currentId = new AtomicLong(0L);
+	private Map<Long, Video> videos = new HashMap<Long, Video>();
+
+	//Adding New Viedo
+	@PostMapping(value = "/video")
+	public @ResponseBody Video addVideo(@RequestBody Video v, HttpServletRequest request) {
+		v.setId(currentId.incrementAndGet());
+		v.setDataUrl(getDataUrl(v.getId()));
+		videos.put(v.getId(), v);
+		return v;
+	}
+
+	//Getting list of Videos
+	@GetMapping(value = "/video")
+	public @ResponseBody List<Video> getVideoList() {
+		return new ArrayList<Video>(videos.values());
+	}
+
+	//Getting Particular video from list
+	@GetMapping(value = "/video/{id}/data")
+	public void getData(@PathVariable("id") long id, HttpServletResponse response) throws IOException {
+		VideoFileManager videoData = VideoFileManager.get();
+		try {
+		videoData.copyVideoData(videos.get(id), response.getOutputStream());
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");
+		}
+	}
+
+	@PostMapping(value = "/video/{id}/data")
+	public @ResponseBody VideoStatus setVideoData(@PathVariable("id") long id,
+			@RequestParam MultipartFile data) throws IOException {
+		VideoFileManager videoData = VideoFileManager.get();
+		try {
+			videoData.saveVideoData(videos.get(id), data.getInputStream());
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");
+		}
+		return new VideoStatus(VideoState.READY);
+	}
+
 	
+	private String getDataUrl(long videoId){
+		String url = getUrlBaseForLocalServer() + "/video/" + videoId + "/data";
+		return url;
+	}
+
+	private String getUrlBaseForLocalServer() {
+		HttpServletRequest request = 
+				((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+		String base = 
+				"http://"+request.getServerName() 
+				+ ((request.getServerPort() != 80) ? ":"+request.getServerPort() : "");
+		return base;
+	}
 }
